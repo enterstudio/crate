e6e57c5dd415b334daa50813c53e3c5e40abbd3d
m-kharbat <meriam.kharbat@gmail.com>
2017-07-13 14:11:03 +0200

    add support for group by with scalar

--- sql/src/main/java/io/crate/analyze/symbol/Aggregations.java
+++ sql/src/main/java/io/crate/analyze/symbol/Aggregations.java
@@ -24,15 +24,60 @@ package io.crate.analyze.symbol;
 
 import io.crate.metadata.FunctionInfo;
 
+import java.util.List;
+
 public final class Aggregations {
 
     private final static AggregationSearcher AGGREGATION_SEARCHER = new AggregationSearcher();
+    private final static AggregationOrScalarSearcher AGGREGATION_OR_SCALAR_SEARCHER = new AggregationOrScalarSearcher();
+    private final static GroupBySymbolMatcher GROUP_BY_MATCHER = new GroupBySymbolMatcher();
 
     /**
      * @return true if the symbol is an aggregation or function which contains an aggregation.
      */
     public static boolean containsAggregation(Symbol s) {
         return AGGREGATION_SEARCHER.process(s, null);
+    }
+
+    /**
+     * @return true if the symbol is an aggregation or function which contains an aggregation or a scalar.
+     */
+    public static boolean containsAggregationOrscalar(Symbol s) {
+        return AGGREGATION_OR_SCALAR_SEARCHER.process(s, null);
+    }
+
+    /**
+     * @return false if the symbol is not found in the group by, or the if one the symbol's non-literal arguments are not found in the group by.
+     */
+    public static boolean matchGroupBySymbol(Symbol s, List<Symbol> groupBy) {
+        return GROUP_BY_MATCHER.process(s, groupBy);
+    }
+
+    private static class AggregationOrScalarSearcher extends SymbolVisitor<Void, Boolean> {
+
+        @Override
+        protected Boolean visitSymbol(Symbol symbol, Void context) {
+            return false;
+        }
+
+        @Override
+        public Boolean visitFunction(Function symbol, Void context) {
+            if (symbol.info().type() == FunctionInfo.Type.AGGREGATE || symbol.info().type() == FunctionInfo.Type.SCALAR) {
+                return true;
+            } else {
+                for (Symbol argument : symbol.arguments()) {
+                    if (process(argument, context)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public Boolean visitAggregation(Aggregation symbol, Void context) {
+            return true;
+        }
     }
 
     private static class AggregationSearcher extends SymbolVisitor<Void, Boolean> {
@@ -58,6 +103,39 @@ public final class Aggregations {
 
         @Override
         public Boolean visitAggregation(Aggregation symbol, Void context) {
+            return true;
+        }
+    }
+
+    private static class GroupBySymbolMatcher extends SymbolVisitor<List<Symbol>, Boolean> {
+
+        @Override
+        protected Boolean visitSymbol(Symbol symbol, List<Symbol> context) {
+            return context.contains(symbol);
+        }
+
+        @Override
+        public Boolean visitLiteral(Literal symbol, List<Symbol> context) {
+            return true;
+        }
+
+        @Override
+        public Boolean visitFunction(Function symbol, List<Symbol> context) {
+            if (context.contains(symbol)) {
+                return true;
+            }
+
+            boolean isSymbolContained = true;
+            if (symbol.info().type() == FunctionInfo.Type.SCALAR) {
+                for (int i = 0; i < symbol.arguments().size(); i++) {
+                    isSymbolContained = isSymbolContained && this.process(symbol.arguments().get(i), context);
+                }
+            }
+            return isSymbolContained;
+        }
+
+        @Override
+        public Boolean visitAggregation(Aggregation symbol, List<Symbol> context) {
             return true;
         }
     }
